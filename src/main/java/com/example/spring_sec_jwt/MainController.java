f93232d51ccc9f46881915cf51fb989a4b6b8558
Kamzs <kamilambroziak@interia.pl>
2019-09-07 15:14:24 +0200

    start project

---
+++ src/main/java/com/example/spring_sec_jwt/MainController.java
@@ -0,0 +1,16 @@
+package com.example.spring_sec_jwt;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String getMainPage ()
+    {
+        return "main page";
+    }
+}
