e140953d11b4ca23011f65253a4549e77774c2c1
Kamzs <kamilambroziak@interia.pl>
2019-09-08 10:51:58 +0200

    added 1. tworzenie uzytkownika 2. określenie dostępów dla grup użytkowników

--- src/main/java/com/example/spring_sec_jwt/MainController.java
+++ src/main/java/com/example/spring_sec_jwt/MainController.java
@@ -7,11 +7,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
 
-    //testssh
     @GetMapping("/")
     @ResponseBody
     public String getMainPage ()
     {
         return "main page";
     }
+
+    //pod tym url (tj. kazdym innym niz powyzszy) dostęp ma tylko admin - tj. dostajemy error 403
+    @GetMapping("/a")
+    @ResponseBody
+    public String getOther ()
+    {
+        return "this page can only be seen by admin";
+    }
+
 }
