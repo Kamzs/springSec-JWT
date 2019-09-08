338f8ae0831d0d2d4aa72374ba5ddc8f6d5fe233
Kamzs <kamilambroziak@interia.pl>
2019-09-08 10:44:12 +0200

    added 1. tworzenie uzytkownika 2. określenie dostępów dla grup użytkowników

--- src/main/java/com/example/spring_sec_jwt/Config.java
+++ src/main/java/com/example/spring_sec_jwt/Config.java
@@ -1,10 +1,51 @@
 package com.example.spring_sec_jwt;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class Config extends WebSecurityConfigurerAdapter {
 
-    //todo - here continue
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        //metoda withDefault... przechowuje login i haslo plain textem
+        //normalnie to powynno byc zaszyfrowane np BCryptem (albo innym algorytmem sluzacym do szyfrowania hasel)
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("user1")
+                .roles("USER")
+                .build();
+        //ponizsze zapisuje uzytkownika w pamieci - w full wersji zapis do bazy danych
+        return new InMemoryUserDetailsManager(userDetails);
+
+    }
+
+    //trzeba nadisac metode configure (http)/override methods /configure
+    //ta metoda sprawdza czy uzytkownik ma uprawnienia wymagane do tego aby otrzymac response na zapytanie na dany URL
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //authorizeRequests() - incializuje analizę requestu
+        http.authorizeRequests()
+        //antMatchers lapie fragment adresu URL zeby sprawdzic czy są spełnione uprawnienia
+        .antMatchers("/")
+        /*możliwe metody to:
+        permitAll - zezwala wszystkim na dostep
+        authenticated - zalogowany
+        hasRole = z daną rolą
+        */
+        .permitAll()
+        //tu można zakończyć ale poniżej dodany fragment mówiący że zapytania na wszystkie inne URL już wymagają roli admina
+        .anyRequest().hasRole("admin")
+        ;
+    }
+
+
 }
